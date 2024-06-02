8c095a4bb9dd83afb44f746cf2936ef22f310ead
Isabele <135793254+isabelesouza@users.noreply.github.com>
2024-06-02 07:21:05 +0000

    teste js

--- js/app.js
+++ js/app.js
@@ -4,20 +4,13 @@ const toggleButtons = document.querySelectorAll('.toggle-resposta');
 // Adicione um ouvinte de evento de clique a cada botão
 toggleButtons.forEach(button => {
   button.addEventListener('click', () => {
-    // Selecione o ID da resposta associada ao botão atual
-    const respostaId = button.dataset.target;
-
     // Selecione a resposta associada ao botão atual
-    const resposta = document.getElementById(respostaId);
+    const resposta = button.parentNode.nextElementSibling;
 
     // Alternar a exibição da resposta
     resposta.classList.toggle('visivel');
 
     // Alterar o texto do botão baseado na visibilidade da resposta
-    if (resposta.classList.contains('visivel')) {
-      button.textContent = '-';
-    } else {
-      button.textContent = '+';
-    }
+    button.textContent = resposta.classList.contains('visivel') ? '-' : '+';
   });
 });
