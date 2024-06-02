44f0747108f0133a7f8ac1f8ad942efdd2806217
Isabele <135793254+isabelesouza@users.noreply.github.com>
2024-06-02 07:27:56 +0000

    mostra resposta

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
-// Selecione todos os botões de alternância
 const toggleButtons = document.querySelectorAll('.toggle-resposta');
 
 // Adicione um ouvinte de evento de clique a cada botão
 toggleButtons.forEach(button => {
   button.addEventListener('click', () => {
+    // Selecione o ID da resposta associada ao botão atual
+    const respostaId = button.dataset.target;
+
     // Selecione a resposta associada ao botão atual
-    const resposta = button.parentNode.nextElementSibling;
+    const resposta = document.getElementById(respostaId);
 
     // Alternar a exibição da resposta
     resposta.classList.toggle('visivel');
 
     // Alterar o texto do botão baseado na visibilidade da resposta
-    button.textContent = resposta.classList.contains('visivel') ? '-' : '+';
+    if (resposta.classList.contains('visivel')) {
+      button.textContent = '-';
+    } else {
+      button.textContent = '+';
+    }
   });
-});   
-
+});
