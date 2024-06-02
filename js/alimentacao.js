e54186af583e239eaa9c72c003bfedc332fd6ba9
Isabele <135793254+isabelesouza@users.noreply.github.com>
2024-06-02 21:02:30 +0000

    mudanças alimentacao

--- js/alimentacao.js
+++ js/alimentacao.js
@@ -1,21 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const tabLinks = document.querySelectorAll(".tab-link");
-  const tabContents = document.querySelectorAll(".tab-content");
+document.addEventListener('DOMContentLoaded', function() {
+  const tabLinks = document.querySelectorAll('.tab-link');
+  const tabContents = document.querySelectorAll('.tab-content');
 
   tabLinks.forEach(link => {
-    link.addEventListener("click", function(e) {
-      e.preventDefault();
-      const tab = this.getAttribute("data-tab");
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+      const tabId = this.getAttribute('data-tab');
 
-      tabLinks.forEach(link => link.classList.remove("active"));
-      tabContents.forEach(content => content.classList.remove("active"));
+      tabContents.forEach(content => {
+        content.classList.remove('active');
+      });
 
-      this.classList.add("active");
-      document.getElementById(tab).classList.add("active");
+      document.getElementById(tabId).classList.add('active');
     });
   });
-
-  // Set default tab
-  document.querySelector(".tab-link[data-tab='doacoes']").classList.add("active");
-  document.getElementById("doacoes").classList.add("active");
 });
