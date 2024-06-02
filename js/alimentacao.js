3f08f7cbf75a4b512eebf862ae737e114f1bb6e9
Isabele <135793254+isabelesouza@users.noreply.github.com>
2024-06-02 20:31:33 +0000

    doações

--- js/alimentacao.js
+++ js/alimentacao.js
@@ -1,17 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const tabs = document.querySelectorAll('.tab-link');
-    const tabContents = document.querySelectorAll('.tab-content');
-  
-    tabs.forEach(tab => {
-      tab.addEventListener('click', function(event) {
-        event.preventDefault();
-        
-        tabs.forEach(tab => tab.classList.remove('active'));
-        tabContents.forEach(content => content.classList.remove('active'));
-  
-        tab.classList.add('active');
-        document.querySelector(`#${tab.dataset.tab}`).classList.add('active');
-      });
+document.addEventListener("DOMContentLoaded", function() {
+  const tabLinks = document.querySelectorAll(".tab-link");
+  const tabContents = document.querySelectorAll(".tab-content");
+
+  tabLinks.forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+      const tab = this.getAttribute("data-tab");
+
+      tabLinks.forEach(link => link.classList.remove("active"));
+      tabContents.forEach(content => content.classList.remove("active"));
+
+      this.classList.add("active");
+      document.getElementById(tab).classList.add("active");
     });
   });
-  
+
+  // Set default tab
+  document.querySelector(".tab-link[data-tab='doacoes']").classList.add("active");
+  document.getElementById("doacoes").classList.add("active");
+});
